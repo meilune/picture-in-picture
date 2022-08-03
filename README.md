20c868c866d8bdcc6d82aa99ecc4277ab2ccf903
Ginja <virginija.meilune@gmail.com>
2022-08-04 09:35:49 +1000

    Adding some style changes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # picture-in-picture
 
 Creating a practice project for picture in picture mode.
+
+The add currently works on Chrome and Safari only.
--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Testing")
+const video = document.getElementById("video");
+const appButton = document.getElementById("button");
+
+console.log("Hello");
+
+  // Hide button if Picture-in-Picture is not supported or disabled.
+//  appButton.hidden = !document.pictureInPictureEnabled || video.disablePictureInPicture;
+
+  appButton.addEventListener("click", async () => {
+    // If there is no element in Picture-in-Picture yet, let’s request
+    // Picture-in-Picture for the video, otherwise leave it.
+    try {
+      if (document.pictureInPictureElement) {
+
+        await document.exitPictureInPicture();
+      } else {
+        video.hidden = false;
+        appButton.hidden = true;
+        await video.requestPictureInPicture();
+      }
+    } catch (err) {
+      // Video failed to enter/leave Picture-in-Picture mode.
+      console.log(err);
+    }
+  });
