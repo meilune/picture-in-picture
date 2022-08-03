1da13161b9a017cc3021bc3b7e0fa3726e013acf
Ginja <virginija.meilune@gmail.com>
2022-08-04 09:58:46 +1000

    Commiting the changes in functionality

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const video = document.getElementById("video");
 const appButton = document.getElementById("button");
 
-console.log("Hello");
-
-  // Hide button if Picture-in-Picture is not supported or disabled.
-//  appButton.hidden = !document.pictureInPictureEnabled || video.disablePictureInPicture;
-
-  appButton.addEventListener("click", async () => {
-    // If there is no element in Picture-in-Picture yet, let’s request
-    // Picture-in-Picture for the video, otherwise leave it.
+async function selectMediaStream() {
     try {
-      if (document.pictureInPictureElement) {
-
-        await document.exitPictureInPicture();
-      } else {
-        video.hidden = false;
-        appButton.hidden = true;
-        await video.requestPictureInPicture();
-      }
-    } catch (err) {
-      // Video failed to enter/leave Picture-in-Picture mode.
-      console.log(err);
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        video.srcObject = mediaStream;
+        video.onloadedmetadata = () => {
+            video.play();
+        }
+    } catch (error) {
+        console.log(`This is the Select Media error: ${error}`);
     }
-  });
+}
+
+appButton.addEventListener("click", async () => {
+    //Disable the button;
+    appButton.disabled = true;
+    //Start Picture in Picture
+    await video.requestPictureInPicture();
+    //Reset the button
+    appButton.disabled = false;
+});
+
+selectMediaStream();
